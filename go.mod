781ab5c930e06364949ed8ad8769a6a002fea489
ruan-duplessis <ruan-duplessis@github.com>
2022-08-05 17:01:13 +0200

    snapshot

--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module github.com/azarc-io/action-module-scrape
 
 go 1.18
 
-require github.com/sethvargo/go-githubactions v1.0.0
+require (
+	github.com/sethvargo/go-githubactions v1.0.0
+	gopkg.in/yaml.v2 v2.4.0
+)
 
-require github.com/sethvargo/go-envconfig v0.6.0 // indirect
+require (
+	github.com/sethvargo/go-envconfig v0.6.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,62 @@
 package main
 
-import ga "github.com/sethvargo/go-githubactions"
+import (
+	"fmt"
+	ga "github.com/sethvargo/go-githubactions"
+	"github.com/xeipuuv/gojsonschema"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+const (
+	sparksRoot = "sparks"
+)
 
 func main() {
-	//action := ga.New()
+	action := ga.New()
 
-	ga.Fatalf("Hello")
+	module := Module{Config: ModuleConfig{}}
+	parseYaml(action, "module.yaml", &module.Config)
+
+	files, err := ioutil.ReadDir(sparksRoot)
+	if err != nil {
+		action.Fatalf("listing sparks", err.Error())
+	}
+
+	for _, dir := range files {
+		if !dir.IsDir() {
+			continue
+		}
+
+		spark := Spark{}
+		module.Sparks = append(module.Sparks, &spark)
+
+		sparkRoot := fmt.Sprintf("%s/%s", sparksRoot, dir.Name())
+		parseYaml(action, fmt.Sprintf("%s/%s", sparkRoot, "spark.yaml"), &spark.Config)
+		loadSchema(action, fmt.Sprintf("%s/%s", sparkRoot, "input_schema.json"), &spark.InputSchema)
+
+		fmt.Println(spark)
+	}
+}
+
+func parseYaml(action *ga.Action, file string, v interface{}) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		action.Fatalf("reading file %s", err.Error())
+	}
+	if err = yaml.Unmarshal(data, v); err != nil {
+		action.Fatalf("unmarshal file %s", err.Error())
+	}
+}
+
+func loadSchema(action *ga.Action, file string, v *[]byte) {
+	var err error
+	*v, err = ioutil.ReadFile(file)
+	if err != nil {
+		action.Fatalf("reading file %s", err.Error())
+	}
+	_, err = gojsonschema.NewSchema(gojsonschema.NewBytesLoader(*v))
+	if err != nil {
+		action.Fatalf("loading schema %s", err.Error())
+	}
 }
