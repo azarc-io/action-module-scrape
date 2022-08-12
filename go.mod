dbeea55c1e8c3a9496a8771e7f40e285c7905e22
ruan-duplessis <ruan-duplessis@github.com>
2022-08-12 14:53:10 +0200

    added sparks to vth module rest module submission

--- go.mod
+++ go.mod
@@ -3,13 +3,22 @@ module github.com/azarc-io/action-module-scrape
 go 1.18
 
 require (
+	github.com/azarc-io/verathread-global v1.24.0
 	github.com/sethvargo/go-githubactions v1.0.0
+	github.com/xeipuuv/gojsonschema v1.2.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
+	github.com/amsokol/protoc-gen-gotag v0.2.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/sethvargo/go-envconfig v0.6.0 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
-	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220222200937-f2425489ef4c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	v1 "github.com/azarc-io/verathread-global/api/v1"
 	ga "github.com/sethvargo/go-githubactions"
 	"github.com/xeipuuv/gojsonschema"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"net/http"
 	"path"
 )
 
@@ -16,9 +20,9 @@ const (
 func main() {
 	action := ga.New()
 	workspace := action.Getenv("GITHUB_WORKSPACE")
+	//action.Getenv("GITHUB_REF_TYPE") !=
 
-	module := Module{Config: ModuleConfig{}}
-
+	module := &v1.Module{Config: &v1.ModuleConfig{}, Repo: action.Getenv("GITHUB_REPOSITORY")}
 	parseYaml(action, fmt.Sprintf("%s/module.yaml", workspace), &module.Config)
 	module.Readme = readFile(action, readme)
 
@@ -33,7 +37,7 @@ func main() {
 			continue
 		}
 
-		spark := Spark{}
+		spark := v1.Spark{}
 		module.Sparks = append(module.Sparks, &spark)
 
 		path.Join()
@@ -43,12 +47,35 @@ func main() {
 		spark.Readme = readFile(action, fmt.Sprintf("%s/%s", sparkRoot, readme))
 	}
 	action.Infof("scraped %d sparks", len(module.Sparks))
+
+	buf := &bytes.Buffer{}
+	if err := json.NewEncoder(buf).Encode(module); err != nil {
+		action.Fatalf("could not encode module: %s", err.Error())
+	}
+	resp, err := http.Post("https://auth-events.cloud.azarc.dev/api/v1/module", "application/json", buf)
+	if err != nil {
+		action.Fatalf("could not add module: %s", err.Error())
+	}
+	if resp.StatusCode != http.StatusOK {
+		action.Fatalf("received %d from add module request", resp.StatusCode)
+	}
+
+	//ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	//conn, err := grpc.DialContext(ctx, ":443", grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//if err != nil {
+	//	action.Fatalf("could not dail verathread: %s", err.Error())
+	//}
+	//fmt.Println("connected")
+	//client := v1.NewModuleSupportClient(conn)
+	//if _, err = client.AddModule(ctx, &v1.AddModuleRequest{Module: module}); err != nil {
+	//	action.Fatalf("could not upload module: %s", err.Error())
+	//}
 }
 
 func readFile(action *ga.Action, file string) []byte {
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
-		action.Fatalf("reading file %s", err.Error())
+		action.Fatalf("reading file: %s", err.Error())
 	}
 	return data
 }
@@ -56,13 +83,13 @@ func readFile(action *ga.Action, file string) []byte {
 func parseYaml(action *ga.Action, file string, v interface{}) {
 	data := readFile(action, file)
 	if err := yaml.Unmarshal(data, v); err != nil {
-		action.Fatalf("unmarshal file %s", err.Error())
+		action.Fatalf("unmarshal file: %s", err.Error())
 	}
 }
 
 func loadSchema(action *ga.Action, file string, v *[]byte) {
 	*v = readFile(action, file)
 	if _, err := gojsonschema.NewSchema(gojsonschema.NewBytesLoader(*v)); err != nil {
-		action.Fatalf("loading schema %s", err.Error())
+		action.Fatalf("loading schema: %s", err.Error())
 	}
 }
